405495630978be993835e40c8c74086160eb5af5
monic <monicaleonor98@gmail.com>
2020-10-15 13:00:49 -0600

    Resolucion preparcial

---
+++ clave_x.py
@@ -0,0 +1,32 @@
+import math
+
+#Ejercicio 1
+def suma():
+    numero1 = 2
+    numero2 = 4
+    suma = numero1 + numero2
+    return suma
+
+
+#Ejercicio 2
+def multiplicacion():
+    numero1 = 2
+    numero2 = 4
+    numero3 = 5
+    multiplicacion = numero1*numero2*numero3
+    return multiplicacion
+
+
+# Ejercicio 3
+def sumarLista():
+    numerosLista = [2,5,4,6,9,12]
+    sumaLista = 0
+    for numberos in numerosLista:
+        sumaLista = sumaLista + numberos
+    return sumaLista
+
+
+
+# Ejercicio 4 github url-->
+def getGithubUrl():
+    return "https://github.com/MonicaLeonor/Preparcial.git"
